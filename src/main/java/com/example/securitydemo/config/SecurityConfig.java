0372b47d1668601a7000817317df75964572963e
sshkim1013 <sshkim1013@naver.com>
2025-11-28 10:01:38 +0900

    1128 custom login

--- src/main/java/com/example/securitydemo/config/SecurityConfig.java
+++ src/main/java/com/example/securitydemo/config/SecurityConfig.java
@@ -14,10 +14,14 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
             .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/", "/info").permitAll()  // '/' 경로와 '/info' 경로는 모두 허용하고
+                    .requestMatchers("/", "/info", "/login").permitAll()  // '/' 경로와 '/info' 경로는 모두 허용하고
                     .anyRequest().authenticated())  // 나머지는 모두 로그인 필요
-            .formLogin(form -> form.permitAll())
-            .logout(logout -> logout.permitAll());
+            .formLogin(form -> form.loginPage("/login")
+                                   .defaultSuccessUrl("/dashboard", true)
+                                   .failureUrl("/login")
+                                   .permitAll())    // 로그인에 성공하면 항상 dashboard로 이동
+            .logout(logout -> logout.logoutSuccessUrl("/login")
+                                    .permitAll());   // 로그아웃에 성공 -> 로그인 페이지로 이동
 
         return httpSecurity.build();
     }
