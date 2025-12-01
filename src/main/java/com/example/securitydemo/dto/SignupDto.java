758f53289560a20a53b3a40c77ab00b1528f760f
sshkim1013 <sshkim1013@naver.com>
2025-12-01 09:24:18 +0900

    1201 SignupDto 작성

---
+++ src/main/java/com/example/securitydemo/dto/SignupDto.java
@@ -0,0 +1,30 @@
+package com.example.securitydemo.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignupDto {
+
+    @NotBlank(message = "사용자명을 입력해주세요.")
+    @Size(min = 3, max = 20, message = "사용자명은 3~20자 사이여야 합니다.")
+    private String username;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Size(min = 4, max = 20, message = "비밀번호는 4~20자 사이여야 합니다.")
+    private String password;
+
+    @NotBlank(message = "비밀번호 확인을 입력해주세요.")
+    private String passwordConfirm;
+
+    // @Email 대신 @Pattern(regexp = "...") 와 같이 정규표현식 사용 가능.
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    private String email;
+
+}
